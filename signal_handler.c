74262b7d97bcaa3d0c6e8675be392e102c4adeb8
meisibley <mkong99@yahoo.com>
2023-08-03 15:57:43 -0700

    no success to fix ctrl-d

--- signal_handler.c
+++ signal_handler.c
@@ -10,7 +10,7 @@ void signal_handler(int signum)
 	char *prompt = "\n($) ";
 	if (signum == SIGINT)
 	{
-		/*if (isatty(STDIN_FILENO))*/
+		if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 	}
 }
